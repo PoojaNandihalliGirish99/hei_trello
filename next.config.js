bc337c79d47f2b9897c59f28a917b8c5029a00d6
Pooja NG <poojan3@poojan3TP253.vmware.com>
2023-10-07 23:18:12 +0530

    modified config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['cloud.appwrite.io','links.papareact.com']
+        domains:['cloud.appwrite.io','links.papareact.com', 'main--chimerical-crumble-974f59.netlify.app']
     }
 }
 
